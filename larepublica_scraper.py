430111f2b9451889ae3f8c36c5163d3eab547773
Martin Ghiena <josemartinghiena@gmail.com>
2020-09-28 21:41:41 -0300

    News scraper finished

--- larepublica_scraper.py
+++ larepublica_scraper.py
@@ -11,6 +11,35 @@ XPATH_SUMMARY = '//div[@class="lead"]/p/text()'
 XPATH_BODY = '//div[@class="html-content"]/p[not(@class)]/text()'
 HOME_URL = 'https://www.larepublica.co/'
 
+def parse_news(link, today):
+    try:
+        response = requests.get(link)
+        if response.status_code == 200:
+            notice = response.content.decode('utf-8')
+            parsed = html.fromstring(notice)
+
+            try:
+                title = parsed.xpath(XPATH_TITLE)[0]
+                title = title.replace('\"', '')
+                summary = parsed.xpath(XPATH_SUMMARY)[0]
+                body = parsed.xpath(XPATH_BODY)
+            except IndexError:
+                return
+            
+            with open(f'{today}/{title}.txt', 'w', encoding='utf-8') as f:
+                f.write(title)
+                f.write('\n\n')
+                f.write(summary)
+                f.write('\n\n')
+                for p in body:
+                    f.write(p)
+                    f.write('\n')
+
+        else:
+            raise ValueError(f'Error: {response.status_code}')
+    except ValueError as ve:
+        print(ve)
+
 def parse_home():
     try:
         response = requests.get(HOME_URL)
@@ -18,8 +47,14 @@ def parse_home():
             home = response.content.decode('utf-8')
             parsed = html.fromstring(home)
             links_to_news = parsed.xpath(XPATH_LINKS_TO_ARTICLE)
-            print(links_to_news)
+            #print(links_to_news)
 
+            today = datetime.date.today().strftime('%d-%m-$Y')
+            if not os.path.isdir(today):
+                os.mkdir(today)
+            
+            for link in links_to_news:
+                parse_news(link, today)
         else:
             raise ValueError(f'Error: {response.status_code}.')
 
